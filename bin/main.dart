1651116526ae1850fc60580f9f0a02cd1443b134
SK <isip_s.k.klesarev@mpt.ru>
2023-03-31 10:45:35 +0300

    api

---
+++ bin/main.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:conduit/conduit.dart';
+import 'package:dart_application_1/dart_application_1.dart';
+
+void main() async {
+  final port = int.parse(Platform.environment["POST"] ?? '8088');
+  final service = Application<AppService>()
+    ..options.port = port
+    ..options.configurationFilePath = 'config.yaml';
+  await service.start(numberOfInstances: 3, consoleLogging: true);
+}
